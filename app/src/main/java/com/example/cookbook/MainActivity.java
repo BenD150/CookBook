cd4d89066bd47120022a14fa48365230098b91fe
BenD150 <dollenmayer.8@osu.edu>
2022-02-24 21:36:47 -0500

    connected homepage and login activities

--- app/src/main/java/com/example/cookbook/MainActivity.java
+++ app/src/main/java/com/example/cookbook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.cookbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 
-    public void disable(View v) {
-
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
+
+
 }
 
