d9ef6f018c26f27e5dbea6a6a0ccbf774193cd62
DaqianZuo <zuo.115@osu.edu>
2022-03-10 21:05:08 -0500

    Implemented User Schema and UploadedRcp class

---
+++ app/src/main/java/com/example/cookbook/UploadedRecipe.java
@@ -0,0 +1,21 @@
+package com.example.cookbook;
+
+import java.util.ArrayList;
+
+public class UploadedRecipe {
+    private String dish, ingredients, directions, description, rcpID;
+    private double rating;
+
+    // Constructor
+    public UploadedRecipe(String dish, String ingredients, String directions, String description){
+        this.dish = dish;
+        this.ingredients = ingredients;
+        this.directions = directions;
+        this.description = description;
+        this.rating = 0.0;
+    }
+
+    public String getRcpID(){
+        return this.rcpID;
+    }
+}
--- app/src/main/java/com/example/cookbook/User.java
+++ app/src/main/java/com/example/cookbook/User.java
@@ -1,16 +1,50 @@
 package com.example.cookbook;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+/*
+    The user class that defines user object to CookBook App.
+ */
+
 public class User {
 
-    public String userName, email;
+    // User information includes username, email address, saved recipe, and uploaded recipe.
+    public String userName, email, UserID;
+    private ArrayList<String> savedRecipe, uploadedRecipe;
 
     public User(){
 
     }
 
+    // Constructor
     public User(String userName, String email){
         this.userName = userName;
         this.email = email;
+        savedRecipe = new ArrayList<String>();
+        uploadedRecipe = new ArrayList<String>();
     }
+
+    // Save recipe to current user's list
+    public void saveRecipe(String recipeID){
+        this.savedRecipe.add(recipeID);
+    }
+
+    // Upload action, create the uploadedrecipe object
+    public void uploadRecipe(String dish, String ingredients, String directions, String description){
+        UploadedRecipe recipe = new UploadedRecipe(dish, ingredients, directions, description);
+        this.uploadedRecipe.add(recipe.getRcpID());
+    }
+
+    // This is the method that check if a certain recipe is created by THIS user.
+    public boolean isCreator (@NonNull UploadedRecipe recipe){
+        boolean indicator = false;
+        if(this.uploadedRecipe.contains(recipe.getRcpID())){
+            indicator = true;
+        }
+        return indicator;
+    }
+
 
 }
