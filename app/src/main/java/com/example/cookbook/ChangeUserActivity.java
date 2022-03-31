efe4b02e3e1e761fa0b87074758b08c7f1c7a3c9
Max <mhathaway225@gmail.com>
2022-03-31 13:52:44 -0400

    started the changePW

--- app/src/main/java/com/example/cookbook/ChangeUserActivity.java
+++ app/src/main/java/com/example/cookbook/ChangeUserActivity.java
@@ -1,5 +1,6 @@
 package com.example.cookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.core.view.Change;
 
 import java.nio.charset.StandardCharsets;
@@ -34,10 +42,36 @@ public class ChangeUserActivity extends AppCompatActivity {
         String userPW = currentPW.getText().toString();
         String newUserPW = newPW.getText().toString();
 
-
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(ChangeUserActivity.this, HomeActivity.class));
+            }
+        });
+
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                AuthCredential credential = EmailAuthProvider.getCredential(userEmail, userPW);
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        user.updatePassword(newUserPW).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ChangeUserActivity.this, "Password has been registered successfully!", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(ChangeUserActivity.this, HomeActivity.class));
+                                } else {
+                                    Toast.makeText(ChangeUserActivity.this, "Failed to change Password.  Please make sure you old password matches the original! Try Again!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                });
+
                 startActivity(new Intent(ChangeUserActivity.this, HomeActivity.class));
             }
         });
