2c38438a50e77ebe325cedb087c5aa25b5158625
BenD150 <dollenmayer.8@osu.edu>
2022-04-15 13:22:54 -0400

    Finished and tested 3 UI/Firebase Tests

--- app/src/androidTest/java/Espresso/EspressoTest.java
+++ app/src/androidTest/java/Espresso/EspressoTest.java
@@ -4,34 +4,31 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
-
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.view.View;
-
-import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.example.cookbook.*;
 import com.example.cookbook.R;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+
 public class EspressoTest {
 
     @Test
-    public void logInWithValidCredentials() {
+    public void firstLogInWithValidCredentials() {
 
         ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
         onView(ViewMatchers.withId(R.id.login)).check(matches(isDisplayed()));
@@ -41,9 +38,10 @@ public class EspressoTest {
         onView(ViewMatchers.withId(R.id.login2)).check(matches(isDisplayed()));
 
         onView(ViewMatchers.withId(R.id.editTextTextEmailAddress2)).perform(ViewActions.typeText("ben@fake.com"));
+
         onView(ViewMatchers.withId(R.id.editTextTextPassword2)).perform(ViewActions.typeText("1234567"));
 
-        ViewActions.closeSoftKeyboard();
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
 
         onView(ViewMatchers.withId(R.id.login2)).perform(ViewActions.click());
 
@@ -51,15 +49,17 @@ public class EspressoTest {
     }
 
     @Test
-    public void addFirebaseRecipe() {
+    public void secondAddFirebaseRecipe() {
 
         ActivityScenario<HomeActivity> activityScenario = ActivityScenario.launch(HomeActivity.class);
+
+        onView(ViewMatchers.withId(R.id.button_upload)).check(matches(isDisplayed()));
 
         onView(ViewMatchers.withId(R.id.button_upload)).perform(ViewActions.click());
 
         onView(ViewMatchers.withId(R.id.addPhoto)).check(matches(isDisplayed()));
 
-        onView(ViewMatchers.withId(R.id.recipeName)).perform(ViewActions.typeText("Espresso"));
+        onView(ViewMatchers.withId(R.id.recipeName)).perform(ViewActions.typeText("Espresso Test"));
 
         onView(ViewMatchers.withId(R.id.prepTime)).perform(ViewActions.typeText("10"));
         onView(ViewMatchers.withId(R.id.cookTime)).perform(ViewActions.typeText("10"));
@@ -67,6 +67,31 @@ public class EspressoTest {
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
 
         onView(ViewMatchers.withId(R.id.createRecipe)).perform(ViewActions.click());
+    }
+
+    @Test
+    public void thirdRemoveSavedRecipe() {
+
+        ActivityScenario<HomeActivity> activityScenario = ActivityScenario.launch(HomeActivity.class);
+
+        onView(ViewMatchers.withId(R.id.button_viewsaved)).check(matches(isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.button_viewsaved)).perform(ViewActions.click());
+
+        onView(ViewMatchers.withId(R.id.mRecyclerView)).check(matches(isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.nameSearch)).check(matches(isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.nameSearch)).perform(ViewActions.typeText("Espresso Test"));
+
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.mRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
+
+        onView(ViewMatchers.withId(R.id.removeBtn)).check(matches(isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.removeBtn)).perform(ViewActions.click());
+
     }
 
 
